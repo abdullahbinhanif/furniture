5f26101b94de276abbf286c62a684a3fc00f5b4f
unknown <abdullahabirweb10@gmail.com>
2024-06-25 20:48:35 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { FaRegUser, FaRegHeart } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white">
+      <div className="py-7 container flex justify-between items-center">
+        <img src="/public/logo.png" alt="logo" />
+        <ul className="flex items-center gap-[75px] text-lg font-primary  font-medium">
+          <li className="text-[#B88E2F]">Home</li>
+          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
+            Shop
+          </li>
+          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
+            About
+          </li>
+          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
+            Contact
+          </li>
+        </ul>
+        <div>
+          <ul className="flex  items-center  gap-10 text-2xl">
+            <li>
+              <FaRegUser />
+            </li>
+            <li>
+              <CiSearch />
+            </li>
+            <li>
+              <FaRegHeart />
+            </li>
+            <li>
+              <FaCartShopping />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;

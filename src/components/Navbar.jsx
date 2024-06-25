cd143390842ac2e7d1785f57f2e520b742fe09a0
unknown <abdullahabirweb10@gmail.com>
2024-06-25 22:14:41 +0600

    banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaRegUser, FaRegHeart } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -9,31 +10,42 @@ const Navbar = () => {
       <div className="py-7 container flex justify-between items-center">
         <img src="/public/logo.png" alt="logo" />
         <ul className="flex items-center gap-[75px] text-lg font-primary  font-medium">
-          <li className="text-[#B88E2F] ">Home</li>
-          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
-            Shop
-          </li>
-          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
-            About
-          </li>
-          <li className=" hover:text-[#B88E2F] transition-all ease-linear">
-            Contact
-          </li>
+          <Link>
+            <li>Home</li>
+          </Link>
+          <Link>
+            <li>Shop</li>
+          </Link>
+          <Link>
+            <li>About</li>
+          </Link>
+          <Link>
+            <li>Contact</li>
+          </Link>
         </ul>
         <div>
           <ul className="flex  items-center  gap-10 text-2xl">
-            <li>
-              <FaRegUser />
-            </li>
-            <li>
-              <CiSearch />
-            </li>
-            <li>
-              <FaRegHeart />
-            </li>
-            <li>
-              <FaCartShopping />
-            </li>
+            <Link>
+              <li>
+                <FaRegUser />
+              </li>
+            </Link>
+
+            <Link>
+              <li>
+                <CiSearch />
+              </li>
+            </Link>
+            <Link>
+              <li>
+                <FaRegHeart />
+              </li>
+            </Link>
+            <Link>
+              <li>
+                <FaCartShopping />
+              </li>
+            </Link>
           </ul>
         </div>
       </div>

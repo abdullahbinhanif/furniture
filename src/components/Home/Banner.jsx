2d3f7f7e4518fd8ce918d144d0c562b1ad3f977b
unknown <abdullahabirweb10@gmail.com>
2024-06-26 21:03:25 +0600

    banner

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className="py-12 bg-[url('/banner.jpg')]">
+    <section className="py-[150px] bg-[url('/banner.jpg')]">
       <div className="container">
-     
+        <div className="py-16 px-12 w-1/2 bg-[#FFF3E3] ml-auto rounded-xl font-primary">
+          <h2 className=" text-lg font-semibold text-primary">New Arrival</h2>
+          <h3 className="text-[52px] text-brand font-bold w-[410px] leading-[65px] pt-[4px] pb-[27px]">
+            Discover Our New Collection
+          </h3>
+          <p className="text-lg text-primary font-medium pb-[46px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis.
+          </p>
+        </div>
       </div>
     </section>
   );

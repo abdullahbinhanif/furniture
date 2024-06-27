dd5f06ae7b56c905335cf9d210ea7bb3bed57fc2
unknown <abdullahabirweb10@gmail.com>
2024-06-27 20:49:19 +0600

    products

--- src/components/Home/OurProducts.jsx
+++ src/components/Home/OurProducts.jsx
@@ -3,12 +3,12 @@ import ProductsItems from "../ProductsItems";
 import { Link } from "react-router-dom";
 const OurProducts = () => {
   return (
-    <section className="pb-16 font-primary">
+    <section className="pb-16 font-primary ">
       <div className="container">
         <h2 className=" text-[32px] text-primary font-bold text-center">
           Our Products
         </h2>
-        <div className=" grid grid-cols-4 gap-8">
+        <div className=" grid grid-cols-4 gap-8 ">
           <ProductsItems />
           <ProductsItems />
           <ProductsItems />

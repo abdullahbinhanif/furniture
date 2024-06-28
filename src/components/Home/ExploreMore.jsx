d532e7cffc56f0491b52b4d5fbbbd66fa78f337f
unknown <abdullahabirweb10@gmail.com>
2024-06-29 00:17:39 +0600

    explore

--- src/components/Home/ExploreMore.jsx
+++ src/components/Home/ExploreMore.jsx
@@ -1,15 +1,20 @@
-import { Pagination, A11y, Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import React, { useRef } from "react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import React from "react";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import NextArrow from "../NextArrow";
 const ExploreMore = () => {
-  const nextRef = useRef();
-  const preRef = <span></span>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <span></span>,
+  };
   return (
-    <section className="mb-16 py-11 bg-[#FCF8F3] ">
+    <section className="mb-16 py-11 bg-[#FCF8F3] explore ">
       <div className="container flex gap-10 items-center">
         <div className="w-2/5">
           <h2 className="text-4xl font-bold font-primary text-[#3A3A3A] w-[410px]">
@@ -24,27 +29,17 @@ const ExploreMore = () => {
           </button>
         </div>
         <div className="w-3/5">
-          <Swiper
-            modules={[Pagination, Navigation, A11y]}
-            spaceBetween={24}
-            slidesPerView={2.3}
-            navigation={{
-              nextEl: nextRef.current,
-            }}
-            pagination={{ clickable: true }}
-            onSlideChange={() => "slide change"}
-            onSwiper={(swiper) => swiper}
-          >
-            <SwiperSlide>
-              <img src="/slide1.png" className="h-[582px] w-full" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="/slide2.png" className="h-[486px] w-full" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="/slide2.png" className="h-[486px] " alt="" />
-            </SwiperSlide>
-          </Swiper>
+          <Slider {...settings}>
+            <div className="slide_items">
+              <img src="/slide1.png" className="w-full h-full" alt="" />
+            </div>
+            <div className="slide_items">
+              <img src="/slide2.png" className="w-full h-full" alt="" />
+            </div>
+            <div className="slide_items">
+              <img src="/slide3.png" className="w-full h-full" alt="" />
+            </div>
+          </Slider>
         </div>
       </div>
     </section>

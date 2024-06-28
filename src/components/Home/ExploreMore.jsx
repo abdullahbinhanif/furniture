5202c5f6713f21be78f673f75b757ae98506d86a
unknown <abdullahabirweb10@gmail.com>
2024-06-28 22:07:02 +0600

    explore

--- src/components/Home/ExploreMore.jsx
+++ src/components/Home/ExploreMore.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+
+import { FaChevronRight } from "react-icons/fa";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
 const ExploreMore = () => {
   return (
     <section className="mb-16 py-11 bg-[#FCF8F3] ">
-      <div className="container">
+      <div className="container flex gap-10 items-center">
         <div className="w-2/5">
           <h2 className="text-4xl font-bold font-primary text-[#3A3A3A] w-[410px]">
             50+ Beautiful rooms inspiration
@@ -14,6 +26,24 @@ const ExploreMore = () => {
           <button className="text-base font-semibold font-primary bg-brand text-white py-[12px] px-9">
             Explore More
           </button>
+        </div>
+        <div className="w-3/5">
+          <Swiper
+            spaceBetween={24}
+            slidesPerView={2.3}
+            onSlideChange={() => "slide change"}
+            onSwiper={(swiper) => swiper}
+          >
+            <SwiperSlide>
+              <img src="/slide1.png" className="h-[582px] w-full" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/slide1.png" className="h-[486px] w-full" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="/slide1.png" alt="" />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>

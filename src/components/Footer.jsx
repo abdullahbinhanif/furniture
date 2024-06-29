9fcd8e28f2ecf3e832b04d59876fbb51dd90da46
unknown <abdullahabirweb10@gmail.com>
2024-06-29 22:02:37 +0600

    browse

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <h1></h1>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Home/Browse.jsx
+++ src/components/Home/Browse.jsx
@@ -3,30 +3,30 @@ const Browse = () => {
   return (
     <section className="py-[56px] ">
       <div className="container font-primary">
-        <div className="pb-16">
-          <h2 className="lg:text-[32px] sm:text-[30px] text-primary font-bold text-center">
+        <div className="lg:pb-16 pb-6">
+          <h2 className="lg:text-[32px] text-xl sm:text-2xl text-primary font-bold text-center">
             Browse The Range
           </h2>
-          <p className="lg:text-xl text-secondary font-normal text-center sm:text-base">
+          <p className="lg:text-xl text-lg   text-secondary font-normal text-center sm:text-base">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
         </div>
-        <div className="lg:flex gap-5">
-          <div className="w-1/3 mx-auto">
+        <div className="lg:flex gap-5 ">
+          <div className="lg:w-1/3 w-2/3 mx-auto">
             <img src="/public/Dining.png" className="w-full" alt="Dining" />
-            <p className=" text-2xl text-primary text-center font-semibold mt-8">
+            <p className=" lg:text-2xl text-primary text-xl  text-center font-semibold  mb-4 lg:mt-8">
               Dining
             </p>
           </div>
-          <div className="w-1/3  mx-auto">
-            <img src="/public/Living.png" className="w-full" alt="Living" />
-            <p className=" text-2xl text-primary text-center font-semibold mt-8 ">
+          <div className="lg:w-1/3 w-2/3 mx-auto">
+            <img src="/public/Living.png" className="w-full " alt="Living" />
+            <p className=" lg:text-2xl text-primary text-xl text-center font-semibold mb-4  lg:mt-8 ">
               Living
             </p>
           </div>
-          <div className="w-1/3 mx-auto">
+          <div className="lg:w-1/3 w-2/3 mx-auto">
             <img src="/public/Bedroom.png" className="w-full" alt="Bedroom" />
-            <p className=" text-2xl text-primary text-center font-semibold mt-8">
+            <p className=" lg:text-2xl text-primary text-xl text-center font-semibold  lg:mt-8">
               Bedroom
             </p>
           </div>

b97bb72850f2d36d7242cd6130744a614ac119b5
unknown <abdullahabirweb10@gmail.com>
2024-06-30 14:11:48 +0600

    button

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Button = ({ title, path }) => {
   return (
     <Link
-      className=" text-white text-base font-bold font-primary py-6 px-[72px] bg-brand inline-block  "
+      className=" text-white text-base font-bold font-primary py-3 px-5 lg:py-6 lg:px-[72px] bg-brand inline-block  "
       to={path}
     >
       {title}
--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -6,13 +6,11 @@ const Banner = () => {
     <section className="py-[150px] md:px-0 lg:px-0 px-5   bg-[url('/banner.jpg')] bg-no-repeat bg-center bg-cover">
       <div className="container  ">
         <div className="pt-14 pb-9 px-10 lg:w-1/2 md:w-1/2   md:mx-80 lg:mx-0 bg-[#FFF3E3] lg:ml-auto rounded-xl font-primary">
-          <h2 className=" text-lg  font-semibold text-primary">
-            New Arrival
-          </h2>
+          <h2 className=" text-lg  font-semibold text-primary">New Arrival</h2>
           <h3 className="lg:text-[52px]  text-3xl  text-brand font-bold  lg:w-[410px] md:w-[250px]  lg:leading-[65px] pt-[4px] pb-[27px]">
             Discover Our New Collection
           </h3>
-          <p className="lg:text-lg md:w-[400px] lg:w-[546px] text-primary font-medium pb-[46px]">
+          <p className="lg:text-lg md:w-[400px]  lg:w-[546px] text-primary font-medium pb-[46px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis.
           </p>

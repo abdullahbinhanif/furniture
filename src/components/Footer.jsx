f14f8254c064aaa1c3d06fe715bee009b016aa1c
unknown <abdullahabirweb10@gmail.com>
2024-06-30 12:08:04 +0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,103 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <>
-      <h1></h1>
-    </>
+    <footer className="pt-12 pb-9 border-t">
+      <div className="container">
+        <div className="container flex justify-between">
+          <div>
+            <img src="/logo.png" alt="" />
+            <p className="text-base font-normal font-primary text-[#9F9F9F] w-[285px] pt-14">
+              400 University Drive Suite 200 Coral Gables, FL 33134 USA
+            </p>
+          </div>
+          <ul>
+            <li>
+              <p className="text-base font-normal font-primary text-[#9F9F9F] pb-14">
+                Links
+              </p>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="font-primary text-base font-medium pb-12 inline-block"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/shop"
+                className="font-primary text-base font-medium pb-12 inline-block"
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className=" font-primary text-base font-medium pb-12 inline-block"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="font-primary text-base font-medium pb-12 inline-block"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <p className="text-base font-normal font-primary text-[#9F9F9F] pb-14">
+                Help
+              </p>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="font-primary text-base font-medium pb-12 inline-block"
+              >
+                Payment Options
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className=" font-primary text-base font-medium pb-12 inline-block"
+              >
+                Returns
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="font-primary text-base font-medium">
+                Privacy Policies
+              </Link>
+            </li>
+          </ul>
+          <div>
+            <p className="text-base font-normal font-primary text-[#9F9F9F] pb-14">
+              Newsletter
+            </p>
+            <input
+              type="text"
+              placeholder="Enter Your Email Address"
+              className="border-b border-b-black outline-none placeholder:text-9F9F9F placeholder:text-[14px] placeholder:font-medium placeholder:font-primary"
+            />
+            <button className="border-b font-primary text-[14px] font-medium border-black ml-2">
+              SUBSCRIBE
+            </button>
+          </div>
+        </div>
+        <p className="font-primary text-base font-normal border-t pt-9">
+          2023 furino. All rights reverved
+        </p>
+      </div>
+    </footer>
   );
 };
 

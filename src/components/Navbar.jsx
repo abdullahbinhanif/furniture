7487a24331a892e750636cb2b51f1fcf48ac00e3
unknown <abdullahabirweb10@gmail.com>
2024-07-01 11:05:53 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaRegUser, FaRegHeart } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { FaCartShopping } from "react-icons/fa6";
-import { RxCross2 } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 import { CgMenuRight } from "react-icons/cg";
 import { Link } from "react-router-dom";
 import logo from "../../public/logo.png";
@@ -39,7 +39,7 @@ const Navbar = () => {
         </div>
         {isopen ? (
           <div className=" ">
-            <ul className="flex flex-col gap-10 absolute top-[73px] left-0 pb-56 w-full h-screen bg-[#f1f1f1]  z-10 items-center text-xl justify-center font-secondary font-semibold">
+            <ul className="flex flex-col gap-10 absolute top-[73px] left-0 pb-56 w-full h-screen bg-white  z-10 items-center text-xl justify-center font-secondary font-semibold">
               <Link>
                 <li onClick={ToggleChange}>Home</li>
               </Link>
@@ -54,10 +54,10 @@ const Navbar = () => {
               </Link>
             </ul>
             <button className="absolute top-[80px] z-10 right-5 py-4 mr-10 cursor-pointer">
-              <RxCross2
+              <IoMdClose
                 onClick={ToggleChange}
                 size={30}
-                className="bg-gray-300 rounded-full p-1"
+                className="bg-slate-200 rounded-full p-1"
               />
             </button>
           </div>
@@ -87,7 +87,7 @@ const Navbar = () => {
             </li>
           </Link>
         </ul>
-        <button onClick={ToggleChange} className="md:hidden pl-10">
+        <button onClick={ToggleChange} className="md:hidden pl-">
           <CgMenuRight size={30} />
         </button>
       </div>
